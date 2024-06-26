1b210779abf2391749534360f5f4b3b60f663094
deividas strole <deiwis@yahoo.com>
2024-06-25 22:01:20 -0700

    progress

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let js = 'amazing';
-if (js == 'amazing') alert('JavaScript is FUN!')
+//if (js == 'amazing') alert('JavaScript is FUN!');
 console.log(40 + 8 + 23 - 10);
+
+console.log("Deividas");
+console.log(23);
+
+let firstName = "Deividas";
+console.log(firstName);
