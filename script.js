a5ebc58fefe9d09154beebd13bfdcad9340033fe
deividas strole <deiwis@yahoo.com>
2024-06-26 20:59:58 -0700

    added some Strings

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ console.log("Population = " + population);
 
 let j = true;
 console.log(typeof j);
+
+const now = 2037;
+const ageDeividas = now - 1978;
+const ageSarah = now - 2018;
+console.log(ageDeividas, ageSarah);
+
+console.log(ageDeividas * 2, ageDeividas / 10, 2 ** 3);
+// 2 ** 3 means 2 to power of 3
+
+const firstName1 = 'Deividas'
+const lastName1 = 'Strole'
+console.log(firstName1 + lastName1);
+
+console.log("I'm " + firstName + ", a " + " programmer.");
